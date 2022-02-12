8559b5f774c8c7ea47ad22b5ca8d4ec198e9ea5b
AnastasiaCymbalyuk <n.wolk@mail.ru>
2022-02-12 19:15:22 +0300

    oop2(fixed)

--- src/js/app.js
+++ src/js/app.js
@@ -36,48 +36,48 @@ export default class Character {
 }
 
 export class Bowman extends Character {
-  constructor(name, type, heaalth, level) {
-    super(name, type, heaalth, level);
+  constructor(name, type) {
+    super(name, type);
     this.attack = 25;
     this.defence = 25;
   }
 }
 
 export class Swordsman extends Character {
-  constructor(name, type, heaalth, level) {
-    super(name, type, heaalth, level);
+  constructor(name, type) {
+    super(name, type);
     this.attack = 40;
     this.defence = 10;
   }
 }
 
 export class Magician extends Character {
-  constructor(name, type, heaalth, level) {
-    super(name, type, heaalth, level);
+  constructor(name, type) {
+    super(name, type);
     this.attack = 10;
     this.defence = 40;
   }
 }
 
 export class Daemon extends Character {
-  constructor(name, type, heaalth, level) {
-    super(name, type, heaalth, level);
+  constructor(name, type) {
+    super(name, type);
     this.attack = 10;
     this.defence = 40;
   }
 }
 
 export class Undead extends Character {
-  constructor(name, type, heaalth, level) {
-    super(name, type, heaalth, level);
+  constructor(name, type) {
+    super(name, type);
     this.attack = 25;
     this.defence = 25;
   }
 }
 
 export class Zombie extends Character {
-  constructor(name, type, heaalth, level) {
-    super(name, type, heaalth, level);
+  constructor(name, type) {
+    super(name, type);
     this.attack = 40;
     this.defence = 10;
   }
